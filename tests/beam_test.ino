c47bcdc1237d3b44aee8606659dac30173034f0e
jillhodges <147772083+jillhodges@users.noreply.github.com>
2026-04-16 16:34:09 -0400

    Update beam_test.ino

--- tests/beam_test.ino
+++ tests/beam_test.ino
@@ -2,13 +2,13 @@
 // 4 row sensors + 4 column sensors = 4x4 grid
 // Detects which row/column intersection was triggered
 
-const int ROW_PINS[4] = {2, 3, 4, 5};
-const int COL_PINS[4] = {6, 7, 8, 9};
-
+const int ROW_PINS[4] = {9, 8, 7, 6};
+const int COL_PINS[4] = {2, 3, 4, 5};
+int h;
 unsigned long lastRowBreak[4] = {0, 0, 0, 0};
 unsigned long lastColBreak[4] = {0, 0, 0, 0};
 
-const int WINDOW = 100; // ms to "remember" a break
+const int WINDOW = 1000; // ms to "remember" a break
 
 void setup() {
   Serial.begin(9600);
@@ -22,11 +22,27 @@ void setup() {
 
 void loop() {
   unsigned long currentTime = millis();
-
+  h=4;
   // Update break timestamps for all rows and columns
   for (int i = 0; i < 4; i++) {
-    if (digitalRead(ROW_PINS[i]) == LOW) lastRowBreak[i] = currentTime;
-    if (digitalRead(COL_PINS[i]) == LOW) lastColBreak[i] = currentTime;
+    //if (digitalRead(COL_PINS[i]) == LOW) lastColBreak[i] = currentTime;
+    if (digitalRead(ROW_PINS[i]) == LOW) {
+        lastRowBreak[i] = currentTime;
+      if (i<h){
+        h = i;
+      }
+      for (int j = 0; j <4; j++){
+        if (digitalRead(COL_PINS[j]) == LOW) {
+          lastColBreak[j] = currentTime;
+          Serial.print("ROW, COL: ");
+          Serial.print(h);
+          Serial.print(", ");
+          Serial.println(j);
+        }
+
+      }
+
+    }
   }
 
   // Check every row/column combination for a coincident break
