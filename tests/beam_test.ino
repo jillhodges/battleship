0cdbd14dbdc57ecc226cb7ca794f9413251c10a2
jillhodges <147772083+jillhodges@users.noreply.github.com>
2026-04-15 23:09:52 -0400

    Update beam_test.ino

--- tests/beam_test.ino
+++ tests/beam_test.ino
@@ -1,50 +1,49 @@
-// Simple IR break-beam example for Arduino
-// 2168 IR break beam sensor (emitter + receiver)
+// IR break-beam grid detection for Arduino Mega
+// 4 row sensors + 4 column sensors = 4x4 grid
+// Detects which row/column intersection was triggered
 
-// Change this pin if you wire the signal to another digital input
-const int ROW_BEAM_PIN = 6;
-const int COL_BEAM_PIN = 7;
-unsigned long lastRowBreak = 0;
-unsigned long lastColBreak = 0;
-const int window = 100; // Time in ms to "remember" a break
+const int ROW_PINS[4] = {2, 3, 4, 5};
+const int COL_PINS[4] = {6, 7, 8, 9};
+
+unsigned long lastRowBreak[4] = {0, 0, 0, 0};
+unsigned long lastColBreak[4] = {0, 0, 0, 0};
+
+const int WINDOW = 100; // ms to "remember" a break
 
 void setup() {
   Serial.begin(9600);
-
-  // Configure pin as input with internal pull-up
-  pinMode(ROW_BEAM_PIN, INPUT_PULLUP);
-  pinMode(COL_BEAM_PIN, INPUT_PULLUP);
-
-  // Optional: built-in LED to indicate beam break
+  for (int i = 0; i < 4; i++) {
+    pinMode(ROW_PINS[i], INPUT_PULLUP);
+    pinMode(COL_PINS[i], INPUT_PULLUP);
+  }
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("4x4 beam grid ready.");
 }
 
 void loop() {
-  bool rowBroken = (digitalRead(ROW_BEAM_PIN) == LOW);
-  bool colBroken = (digitalRead(COL_BEAM_PIN) == LOW);
   unsigned long currentTime = millis();
 
-  // Record the timestamp of the most recent break
-  if (rowBroken) lastRowBreak = currentTime;
-  if (colBroken) lastColBreak = currentTime;
+  // Update break timestamps for all rows and columns
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(ROW_PINS[i]) == LOW) lastRowBreak[i] = currentTime;
+    if (digitalRead(COL_PINS[i]) == LOW) lastColBreak[i] = currentTime;
+  }
 
-  // Check if BOTH have been broken within the 'window'
-  if ((currentTime - lastRowBreak < window) && (currentTime - lastColBreak < window)) {
-    Serial.println("INTERSECTION: Ball Detected!");
-    
-    // Reset timers so we don't spam the "Intersection" message
-    lastRowBreak = 0; 
-    lastColBreak = 0;
-  } 
-//  else if (rowBroken) {
-//    // Only print if the other beam hasn't been hit recently
-//    if (currentTime - lastColBreak > window) {
-//      Serial.println("Row break");
-//    }
-//  } 
-//  else if (colBroken) {
-//    if (currentTime - lastRowBreak > window) {
-//      Serial.println("Column break");
-//    }
-//  }
+  // Check every row/column combination for a coincident break
+  for (int r = 0; r < 4; r++) {
+    for (int c = 0; c < 4; c++) {
+      if ((currentTime - lastRowBreak[r] < WINDOW) &&
+          (currentTime - lastColBreak[c] < WINDOW)) {
+
+        Serial.print("INTERSECTION DETECTED: Row ");
+        Serial.print(r + 1);
+        Serial.print(", Column ");
+        Serial.println(c + 1);
+
+        // Reset both so we don't spam the same hit
+        lastRowBreak[r] = 0;
+        lastColBreak[c] = 0;
+      }
+    }
+  }
 }
