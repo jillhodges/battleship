8c80f53a3b9b7849ec23872e1c997411332f420d
jillhodges <147772083+jillhodges@users.noreply.github.com>
2026-04-21 19:43:28 -0400

    Update mainESP.ino

--- mainESP.ino
+++ mainESP.ino
@@ -1,1 +1,1 @@
-//include code for LCD, servos, playstation controller, UART comms with MEGA
+//include code for LCD, servos, playstation controller, UART comms with MEGA, potential laser pointer (2 digital pins)
