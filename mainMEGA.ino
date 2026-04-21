590c908cf5171e0d458b8521589cf4d4b100b30a
jillhodges <147772083+jillhodges@users.noreply.github.com>
2026-04-21 19:42:51 -0400

    Update mainMEGA.ino

--- mainMEGA.ino
+++ mainMEGA.ino
@@ -1,1 +1,6 @@
-// include code for microswitches, beam breaks, UART comms with esp32
+// include code for microswitches, beam breaks, LEDs, UART comms with esp32
+// pins 2-9: beam breaks
+// pins 10-13: LEDs
+//pins 22-38: defender board 1
+// pins 39-54: defender board 2
+// pins 0-1: RX-TX comms with ESP32
