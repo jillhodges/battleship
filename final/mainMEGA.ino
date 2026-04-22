92ac0ff9d6751d8cd58d82fb6fedd7ced2472e10
jillhodges <147772083+jillhodges@users.noreply.github.com>
2026-04-21 20:31:11 -0400

    Update mainMEGA.ino

--- final/mainMEGA.ino
+++ final/mainMEGA.ino
@@ -5,13 +5,46 @@
 // pins 39-54: defender board 2
 // pins 0-1: RX-TX comms with ESP32
 
-#include "BeamGrid.h"    // 1. include at the top
+// INCLUDE ALL LIBRARIES
+#include "BeamGrid.h"    
+#include <Adafruit_NeoPixel.h>
+#include "ShipPlacement.h"
 
+// ALL OBJECTS:
 BeamGrid grid;           // 2. create the object, global scope
 
 void setup() {
-  Serial.begin(9600);
-  grid.begin();          // 3. call in setup()
+  Serial.begin(9600);  // debug output
+  Serial1.begin(9600);      // communication with ESP32 (TX1/RX1 pins 18/19)
+
+  grid.begin();          // beam break initialization
+  
+  // LED initialization
+  p1Strip.begin();
+  p1Strip.setBrightness(80);
+  p1Strip.clear();
+  p1Strip.show();
+
+  p2Strip.begin();
+  p2Strip.setBrightness(80);
+  p2Strip.clear();
+  p2Strip.show();
+
+  //defender board placement time
+  // Microswitch pins - P1 board
+  for (int r = 0; r < ROWS; r++)
+    for (int c = 0; c < COLS; c++)
+      pinMode(P1_PINS[r][c], INPUT_PULLUP);
+
+  // Microswitch pins - P2 board
+  for (int r = 0; r < ROWS; r++)
+    for (int c = 0; c < COLS; c++)
+      pinMode(P2_PINS[r][c], INPUT_PULLUP);
+
+  // Ship placement runs here and blocks until both players are done
+  runPlacementPhase();
+
+
 }
 
 void loop() {
